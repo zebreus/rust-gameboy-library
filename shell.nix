6b03716c63301f1e0cbe2df562a712e1bb297800
Zebreus <lennarteichhorn@googlemail.com>
2022-11-28 09:28:54 +0000

    Setup rust and nix for codespaces

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/release-21.11.tar.gz") { } }:
+
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs; [
+    nixpkgs-fmt
+    rnix-lsp
+    docker-client
+    gnumake
+
+    # go development
+    rustc
+    cargo
+  ];
+
+  hardeningDisable = [ "all" ];
+}
