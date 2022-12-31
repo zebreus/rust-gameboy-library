e000e251c37c1f9b6205b50bebf64128dfb23564
Lennart <lennarteichhorn@googlemail.com>
2022-12-31 16:01:13 +0000

    Shorten timeout for instructions test

--- src/test_roms/blargg/instruction_timing.rs
+++ src/test_roms/blargg/instruction_timing.rs
@@ -4,6 +4,6 @@ mod tests {
 
     #[test]
     fn all_instructions_test() {
-        test_blargg_rom("test_roms/blargg/instr_timing/instr_timing.gb", 1000000000);
+        test_blargg_rom("test_roms/blargg/instr_timing/instr_timing.gb", 1000000);
     }
 }
