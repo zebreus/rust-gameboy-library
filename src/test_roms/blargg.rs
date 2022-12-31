4b4de50ecd05cf583643560d6d97796ef2088258
Lennart <lennarteichhorn@googlemail.com>
2022-12-31 15:22:47 +0000

    Add instruction timing test

--- src/test_roms/blargg.rs
+++ src/test_roms/blargg.rs
@@ -1,3 +1,4 @@
+mod instruction_timing;
 mod instructions;
 
 #[cfg(test)]
---
+++ src/test_roms/blargg/instruction_timing.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod tests {
+    use super::super::test_blargg_rom;
+
+    #[test]
+    fn all_instructions_test() {
+        test_blargg_rom("test_roms/blargg/instr_timing/instr_timing.gb", 1000000000);
+    }
+}
