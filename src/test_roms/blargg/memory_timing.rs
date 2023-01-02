78ca6fb65368d83e90fe3210e1b445d4cff7de12
Lennart <lennarteichhorn@googlemail.com>
2023-01-02 14:34:30 +0000

    Executed combined memory timing test

--- src/test_roms/blargg/memory_timing.rs
+++ src/test_roms/blargg/memory_timing.rs
@@ -3,6 +3,11 @@ mod tests {
     use super::super::test_blargg_rom;
 
     #[test]
+    fn memory_timing_full_test() {
+        test_blargg_rom("test_roms/blargg/mem_timing/mem_timing.gb", 10000000);
+    }
+
+    #[test]
     fn memory_read_timing_test() {
         test_blargg_rom(
             "test_roms/blargg/mem_timing/individual/01-read_timing.gb",
