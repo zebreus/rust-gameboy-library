198540c23826e4fa41613ea182d9364b547d3be8
Lennart <lennarteichhorn@googlemail.com>
2023-01-05 12:28:52 +0000

    Implement png display

--- src/memory/video/display_connection.rs
+++ src/memory/video/display_connection.rs
@@ -1,3 +1,5 @@
+use image::{ImageBuffer, Rgba, RgbaImage};
+
 /// The trait is used to connect to a display
 pub trait DisplayConnection {
     /// Define the current color of a pixel
@@ -15,22 +17,41 @@ impl DisplayConnection for DummyDisplayConnection {
 }
 
 /// A display connection that creates a png for each frame
-pub struct PngDisplayConnection {}
+pub struct PngDisplayConnection {
+    image: RgbaImage,
+}
 
 impl PngDisplayConnection {
     /// Create a new png display connection
     pub fn new() -> PngDisplayConnection {
-        PngDisplayConnection {}
+        PngDisplayConnection {
+            image: ImageBuffer::new(160, 144),
+        }
     }
 }
 
 impl DisplayConnection for PngDisplayConnection {
     /// Define the current color of a pixel
-    fn set_pixel(&mut self, _x: usize, _y: usize, _value: (u8, u8, u8, u8)) {
-        todo!();
+    fn set_pixel(&mut self, x: usize, y: usize, value: (u8, u8, u8, u8)) {
+        let (red, green, blue, alpha) = value;
+        self.image
+            .put_pixel(x as u32, y as u32, Rgba([red, green, blue, alpha]))
     }
     /// Notify the display that the current frame has finished drawing
     fn finish_frame(&mut self) {
-        todo!();
+        self.image.save("test.png").unwrap();
+        self.image = ImageBuffer::new(160, 144);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{DisplayConnection, PngDisplayConnection};
+
+    #[test]
+    fn test_png_display() {
+        let mut png_display = PngDisplayConnection::new();
+        png_display.set_pixel(2, 2, (127, 127, 127, 127));
+        png_display.finish_frame();
     }
 }
